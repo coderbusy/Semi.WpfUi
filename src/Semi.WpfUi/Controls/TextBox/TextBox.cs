0a0e90fd5409ae941b399c75c84c46e6ba5a8981
Soar360 <Soar360@live.com>
2026-07-10 14:31:34 +0800

    自定义TextBox控件，支持占位符与内嵌内容
    新增Semi.WpfUi.Controls.TextBox，继承自系统TextBox，增加InnerLeftContent和InnerRightContent依赖属性用于左右内嵌内容，支持PlaceholderText占位符文本及PlaceholderForegroundProperty占位符颜色属性，提升控件扩展性和可用性。

---
+++ src/Semi.WpfUi/Controls/TextBox/TextBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Semi.WpfUi.Controls;
+
+public class TextBox : System.Windows.Controls.TextBox
+{
+    public static readonly DependencyProperty InnerLeftContentProperty = DependencyProperty.Register(
+        nameof(InnerLeftContent), typeof(object), typeof(TextBox), new FrameworkPropertyMetadata(null));
+
+    public static readonly DependencyProperty InnerRightContentProperty = DependencyProperty.Register(
+        nameof(InnerRightContent), typeof(object), typeof(TextBox), new FrameworkPropertyMetadata(null));
+
+
+    public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(
+        nameof(PlaceholderText), typeof(string), typeof(TextBox), new PropertyMetadata(default(string)));
+
+    public static readonly DependencyProperty PlaceholderForegroundPropertyProperty =
+        DependencyProperty.Register(nameof(PlaceholderForegroundProperty), typeof(Brush), typeof(TextBox), new PropertyMetadata(SystemColors.GrayTextBrush));
+
+    public Brush PlaceholderForegroundProperty
+    {
+        get { return (Brush)GetValue(PlaceholderForegroundPropertyProperty); }
+        set { SetValue(PlaceholderForegroundPropertyProperty, value); }
+    }
+
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
+
+    public object? InnerLeftContent
+    {
+        get => GetValue(InnerLeftContentProperty);
+        set => SetValue(InnerLeftContentProperty, value);
+    }
+
+    public object? InnerRightContent
+    {
+        get => GetValue(InnerRightContentProperty);
+        set => SetValue(InnerRightContentProperty, value);
+    }
+}
