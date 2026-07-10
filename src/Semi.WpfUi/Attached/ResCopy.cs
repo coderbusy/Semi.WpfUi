3c6c2b496172add3f3192edeaa71fa88817393ce
Soar360 <Soar360@live.com>
2026-07-10 11:42:54 +0800

    重构TextBlock资源定义，新增ResCopy附加属性
    重构了 TextBlock.xaml 的资源定义方式，部分颜色资源通过自定义的 attached:ResCopy.From 实现颜色复制，提升资源复用性。新增 ResCopy.cs，实现颜色复制的附加属性类。新增 TextBlock.cs，定义资源字典局部类并重写 OnGettingValue 方法，为后续扩展做准备。

---
+++ src/Semi.WpfUi/Attached/ResCopy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Semi.WpfUi.Attached;
+
+public static class ResCopy
+{
+    public static readonly DependencyProperty FromProperty = DependencyProperty.RegisterAttached(
+        "From", typeof(object), typeof(ResCopy), new PropertyMetadata(null, OnFromChanged));
+
+    private static void OnFromChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is SolidColorBrush brush)
+        {
+            if (e.NewValue is SolidColorBrush originalBrush)
+            {
+                brush.Color = originalBrush.Color;
+            }
+        }
+    }
+
+    public static void SetFrom(DependencyObject element, object value)
+        => element.SetValue(FromProperty, value);
+
+    public static object GetFrom(DependencyObject element)
+        => element.GetValue(FromProperty);
+}
---
+++ src/Semi.WpfUi/Themes/Light/TextBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Semi.WpfUi.Themes.Light;
+
+public partial class TextBlock : ResourceDictionary
+{
+    protected override void OnGettingValue(object key, ref object value, out bool canCache)
+    {
+        
+        base.OnGettingValue(key, ref value, out canCache);
+    }
+}
